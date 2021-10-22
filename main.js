4d62fc5942d35c55548a382289c073b170eb34e5
Diogo Eugênio <72772897+di-eug@users.noreply.github.com>
2021-10-22 11:43:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'di-eug',
+  youtube: 'channel/UCgD0_aZg1HgmWO88MOjqYYQ',
+  facebook: '#',
+  instagram: 'diovoovo',
+  twitter: 'di_ovo'
+}
+
+function ChangeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+ChangeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
